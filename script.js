515b6070ed254375257560e4eb992b3e98038db3
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-08-05 12:23:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ function filterProjects(category) {
     var projects = document.getElementsByClassName('project-item');
     for (var i = 0; i < projects.length; i++) {
         if (category === 'all') {
-            projects[i].classList.add('show');
+            projects[i].style.display = 'block';
         } else {
             if (projects[i].getAttribute('data-category') === category) {
-                projects[i].classList.add('show');
+                projects[i].style.display = 'block';
             } else {
-                projects[i].classList.remove('show');
+                projects[i].style.display = 'none';
             }
         }
     }
 }
-
-// Show all projects by default
-filterProjects('all');
